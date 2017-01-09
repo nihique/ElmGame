4375d5dbb64ec9960b540f7c6783ccbdcf58c54f
nihique <kovarik.martin@gmail.com>
2017-01-09 17:06:27 +0100

    widget can compile

--- ChildParent/Widget.elm
+++ ChildParent/Widget.elm
@@ -5,11 +5,13 @@ import Html.Attributes exposing (type_)
 import Html.Events exposing (onClick)
 
 type alias Model =
-    { count : int }
+    { count : Int
+    }
 
 initialModel : Model
 initialModel =
-    { count = 1 }
+    { count = 1
+    }
 
 
 type Msg
@@ -20,19 +22,19 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment
-            -> { model | count = model.count + 1 }
+            -> ( { model | count = model.count + 1 }, Cmd.none )
 
         Decrement
-            -> { model | count = model.count - 1 }
+            -> ( { model | count = model.count - 1 }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     div
         []
-        [ text model.count
-        , input [ type_ "button" onClick Decrement ] [ text "Decrement" ]
-        , input [ type_ "button" onClick Increment ] [ text "Increment" ]
+        [ text (toString model.count)
+        , input [ type_ "button", onClick Decrement ] [ text "Decrement" ]
+        , input [ type_ "button", onClick Increment ] [ text "Increment" ]
         ]
 
 
